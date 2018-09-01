29df540e31965158f4e755d863baab29b2dd05d0
mmwalker1986 <mmwalker1986@gmail.com>
2018-09-01 13:12:48 +0100

    Evolved from procedule program to Object orientated

--- Enigma_Cipher/Enigma_Cipher.h
+++ Enigma_Cipher/Enigma_Cipher.h
@@ -8,11 +8,32 @@ using std::cin;
 using std::endl;
 
 
-void delete_message(string &the_message);
-string change_rotors(int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, string &rotor_1, string &rotor_2, string &rotor_3, const string &original_rotor_1, const string &original_rotor_2, const string &original_rotor_3);
-bool quit_program(bool &quit);
-void main_menu(string &the_message, int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, char letter);
-char letter_to_uppercase();
-string create_message(string &the_message);
-string encrypt_message(string &the_message, string &rotor_1, string &rotor_2, string &rotor_3, int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, const string original_rotor_1, const string original_rotor_2, const string original_rotor_3);
-string decrypt_message(string &the_message, string &rotor_1, string &rotor_2, string &rotor_3, int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, const string original_rotor_1, const string original_rotor_2, const string original_rotor_3);
+class Enigma_Cipher {
+public:
+	char letter_to_uppercase();
+	bool quit_program(bool &quit);
+	void main_menu(string &the_message, int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, char letter);
+	void delete_message(string &the_message);
+	string create_message(string &the_message);
+	string change_rotors(int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, string &rotor_1, string &rotor_2, string &rotor_3, const string &original_rotor_1, const string &original_rotor_2, const string &original_rotor_3);
+	string encrypt_message(string &the_message, string &rotor_1, string &rotor_2, string &rotor_3, int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, const string original_rotor_1, const string original_rotor_2, const string original_rotor_3);
+	string decrypt_message(string &the_message, string &rotor_1, string &rotor_2, string &rotor_3, int &rotor_pos_1, int &rotor_pos_2, int &rotor_pos_3, const string original_rotor_1, const string original_rotor_2, const string original_rotor_3);
+
+	//76 characters
+	const string original_rotor_1{ "CTr9Y+k!_(LlzSn6Bt3DdGf^Js=q1I o0NUj8cE7&g)HiOeRw2WAbQ$pV*h-%xyXF?Z5PMuvma4K" };
+	const string original_rotor_2{ "a+?A3uNfQTKnVgD$F6P^yw1kB9Cqs)Hcl!-MRdZ%x5*t8Lp0S7vr=eIJhzjGW_X&i4m 2O(EUboY" };
+	const string original_rotor_3{ "EG)sQn&IfRJ +cp%?m(iPdgb!wlqzLHSW-xAK54v3aFBrDyUet9o18kMVYj^Th$*7OXZ=26_0NCu" };
+	string rotor_1{ "CTr9Y+k!_(LlzSn6Bt3DdGf^Js=q1I o0NUj8cE7&g)HiOeRw2WAbQ$pV*h-%xyXF?Z5PMuvma4K" };
+	string rotor_2{ "a+?A3uNfQTKnVgD$F6P^yw1kB9Cqs)Hcl!-MRdZ%x5*t8Lp0S7vr=eIJhzjGW_X&i4m 2O(EUboY" };
+	string rotor_3{ "EG)sQn&IfRJ +cp%?m(iPdgb!wlqzLHSW-xAK54v3aFBrDyUet9o18kMVYj^Th$*7OXZ=26_0NCu" };
+	char letter{};
+	int rotor_pos_1{ 1 };
+	int rotor_pos_2{ 1 };
+	int rotor_pos_3{ 1 };
+	bool quit{ false };
+	string the_message{ "" };
+
+
+private:
+
+};
---
+++ Enigma_Cipher/main.cpp
@@ -0,0 +1,60 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "Enigma_Cipher.h"
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+	Enigma_Cipher Message;
+
+	while (Message.quit != true) {
+		cout << Message.rotor_1 << endl;
+		cout << Message.rotor_2 << endl;
+		cout << Message.rotor_3 << endl;
+		Message.main_menu(Message.the_message, Message.rotor_pos_1, Message.rotor_pos_2, Message.rotor_pos_3, Message.letter);
+		Message.letter = Message.letter_to_uppercase();
+
+		switch (Message.letter) {
+		case 'C':
+		{
+			Message.create_message(Message.the_message);
+			break;
+		}
+		case 'E':
+		{
+			Message.encrypt_message(Message.the_message, Message.rotor_1, Message.rotor_2, Message.rotor_3, Message.rotor_pos_1, Message.rotor_pos_2, Message.rotor_pos_3, Message.original_rotor_1, Message.original_rotor_2, Message.original_rotor_3);
+			break;
+		}
+		case 'X':
+		{
+			Message.delete_message(Message.the_message);
+			break;
+		}
+		case 'S':
+		{
+			Message.change_rotors(Message.rotor_pos_1, Message.rotor_pos_2, Message.rotor_pos_3, Message.rotor_1, Message.rotor_2, Message.rotor_3, Message.original_rotor_1, Message.original_rotor_2, Message.original_rotor_3);
+			break;
+		}
+
+		case 'D':
+		{
+			Message.decrypt_message(Message.the_message, Message.rotor_1, Message.rotor_2, Message.rotor_3, Message.rotor_pos_1, Message.rotor_pos_2, Message.rotor_pos_3, Message.original_rotor_1, Message.original_rotor_2, Message.original_rotor_3);
+			break;
+
+		}
+		case 'Q':
+		{
+			Message.quit_program(Message.quit);
+			break;
+		}
+		default:
+			cout << "Enter one of the valid letters please" << endl;
+		}
+		cout << endl;
+	}
+	return 0;
+}
